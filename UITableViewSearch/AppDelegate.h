6471b47aab4b913d2b76d1541eda2d9a8682a95a
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-25 16:07:03 +0300

    Initial Commit

---
+++ UITableViewSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewSearch
+//
+//  Created by EnzoF on 25.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewSearch
+//
+//  Created by EnzoF on 25.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
