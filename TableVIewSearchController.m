ead07d9ef1fb3e5a836946e9273915c7c1d1087b
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-26 19:08:20 +0300

    level master completed

--- TableVIewSearchController.m
+++ TableVIewSearchController.m
@@ -11,83 +11,65 @@
 #import "StudentCell.h"
 #import "SectionMonth.h"
 
-@interface TableVIewSearchController()<UITableViewDelegate,UITableViewDataSource>
+@interface TableVIewSearchController()<UITableViewDelegate,UITableViewDataSource,UISearchBarDelegate>
 
 @property (strong,nonatomic) NSArray *arrayStudents;
 @property (strong,nonatomic) NSDateFormatter *dateFormatter;
 @property (strong,nonatomic) NSArray *arraySectionMonth;
-
+@property (strong,nonatomic) NSArray *arrayFilteredSectionMonth;
+@property (strong,nonatomic) NSOperationQueue *opQueue;
 @end
 
 @implementation TableVIewSearchController
 
 -(void)viewDidLoad{
     
-    NSInteger numberOfStudents = 20;
-    
+    NSInteger numberOfStudents = 30000;
+    NSInteger fromAge = 18;
+    NSInteger toAge = 23;
+
     NSMutableArray<Student*> *tempMArrayStudents = [[NSMutableArray alloc]init];
     for(NSInteger i = 0; i < numberOfStudents;i++)
     {
-        Student *currentStudent = [[Student alloc]initRandomStudentWithDateOfBirthOfFromAge:18 toAge:23];
+        Student *currentStudent = [[Student alloc]initRandomStudentWithDateOfBirthOfFromAge:fromAge toAge:toAge];
         [tempMArrayStudents addObject:currentStudent];
     }
     
-    NSSortDescriptor *sortDescriptorStudent = [[NSSortDescriptor alloc]initWithKey:@"self" ascending:YES comparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
-        Student *student1 = (Student*)obj1;
-        Student *student2 = (Student*)obj2;
-        if([student1.firstName compare:student2.firstName] == NSOrderedSame)
-        {
-            return [student1.lastName compare:student2.lastName];
-        }
-        return [student1.firstName compare:student2.firstName];
+    __weak TableVIewSearchController *weakSelf = self;
+    NSOperation *sortOperation = [NSBlockOperation blockOperationWithBlock:^{
         
-    }];
-    
-    NSSortDescriptor *sortDescriptorMonth = [[NSSortDescriptor alloc]initWithKey:@"self" ascending:YES comparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
-        Student *student1 = (Student*)obj1;
-        Student *student2 = (Student*)obj2;
-        NSDateFormatter *dateMonthFormatter = [[NSDateFormatter alloc]init];
-        [dateMonthFormatter setDateFormat:@"MM"];
-        NSString *monthStudent1 = [dateMonthFormatter stringFromDate:student1.dayOfBirth];
-        NSString *monthStudent2 = [dateMonthFormatter stringFromDate:student2.dayOfBirth];
-        return [monthStudent1 compare:monthStudent2];
-    }];
-    
-    [tempMArrayStudents sortUsingDescriptors:@[sortDescriptorStudent]];
-    self.arrayStudents = [tempMArrayStudents sortedArrayUsingDescriptors:@[sortDescriptorMonth]];
-    
-    
-    
-    NSDateFormatter *dateMonthFormatter = [[NSDateFormatter alloc]init];
-    NSMutableArray *mArraySectionMonth = nil;
-    NSMutableArray *mArrayItems = nil;
-    NSString *currentMonth = nil;
-    SectionMonth *sectionMonth = nil;
-    [dateMonthFormatter setDateFormat:@"MMMM"];
-    
-    for (Student *currentStudent in self.arrayStudents)
-    {
-        NSString *month = [dateMonthFormatter stringFromDate:currentStudent.dayOfBirth];
+        NSSortDescriptor *sortDescriptorStudent = [[NSSortDescriptor alloc]initWithKey:@"self" ascending:YES comparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+            Student *student1 = (Student*)obj1;
+            Student *student2 = (Student*)obj2;
+            if([student1.firstName compare:student2.firstName] == NSOrderedSame)
+            {
+                return [student1.lastName compare:student2.lastName];
+            }
+            return [student1.firstName compare:student2.firstName];
+            
+        }];
         
-        if(!mArraySectionMonth)
-        {
-            mArraySectionMonth = [[NSMutableArray alloc]init];
-        }
-
-        if(![month isEqualToString:currentMonth])
-        {
-            sectionMonth = [[SectionMonth alloc]init];
-            sectionMonth.titleOfHeader = month;
-            [mArraySectionMonth addObject:sectionMonth];
-            mArrayItems = [[NSMutableArray alloc]init];
-            currentMonth = month;
-        }
-        [mArrayItems addObject:currentStudent];
-        sectionMonth.arrayItems = [[NSArray alloc]initWithArray:mArrayItems];
-    }
-    
-    self.arraySectionMonth = [[NSArray alloc]initWithArray:mArraySectionMonth];
-    
+        NSSortDescriptor *sortDescriptorMonth = [[NSSortDescriptor alloc]initWithKey:@"self" ascending:YES comparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+            Student *student1 = (Student*)obj1;
+            Student *student2 = (Student*)obj2;
+            NSDateFormatter *dateMonthFormatter = [[NSDateFormatter alloc]init];
+            [dateMonthFormatter setDateFormat:@"MM"];
+            NSString *monthStudent1 = [dateMonthFormatter stringFromDate:student1.dayOfBirth];
+            NSString *monthStudent2 = [dateMonthFormatter stringFromDate:student2.dayOfBirth];
+            return [monthStudent1 compare:monthStudent2];
+        }];
+        
+        [tempMArrayStudents sortUsingDescriptors:@[sortDescriptorStudent]];
+        weakSelf.arrayStudents = [tempMArrayStudents sortedArrayUsingDescriptors:@[sortDescriptorMonth]];
+        NSArray *arrayMonthSections = [weakSelf sortArrayFromArray:self.arrayStudents];
+        dispatch_queue_t queue = dispatch_get_main_queue();
+        dispatch_async(queue, ^{
+            weakSelf.arraySectionMonth = arrayMonthSections;
+            [weakSelf.tableView reloadData];
+        });
+    }];
+    self.opQueue = [[NSOperationQueue alloc] init];
+    [self.opQueue addOperation:sortOperation];
 }
 
 #pragma mark - lazy Initialization
@@ -112,7 +94,6 @@
 
 
 #pragma mark - UITableViewDataSource
-
 - (nullable NSArray<NSString *> *)sectionIndexTitlesForTableView:(UITableView *)tableView{
     NSMutableArray<NSString*> *mArrayShortMountsName;
     for(SectionMonth *currentSection in self.arraySectionMonth)
@@ -155,6 +136,85 @@
 - (nullable NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
     
     return [[self.arraySectionMonth objectAtIndex:section] titleOfHeader];
+}
+
+
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar{
+    [searchBar setShowsCancelButton:YES animated:YES];
+    
+}
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar{
+    [searchBar resignFirstResponder];
+    [searchBar setShowsCancelButton:NO animated:YES];
+}
+
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText{
+    
+    [self.opQueue cancelAllOperations];
+    __weak TableVIewSearchController *weakSelf = self;
+    NSOperation *filterOp = [NSBlockOperation blockOperationWithBlock:^{
+        NSArray *arrayFilteredSections =  [weakSelf sortArrayFromArray:weakSelf.arrayStudents withFilter:searchText];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            weakSelf.arraySectionMonth = arrayFilteredSections;
+            [weakSelf.tableView reloadData];
+        });
+    }];
+    [self.opQueue addOperation:filterOp];
+}
+#pragma mark - metods
+
+-(NSArray*)sortArrayFromArray:(NSArray*)arrayStudents{
+    NSDateFormatter *dateMonthFormatter = [[NSDateFormatter alloc]init];
+    NSMutableArray *mArraySectionMonth = nil;
+    NSMutableArray *mArrayItems = nil;
+    NSString *currentMonth = nil;
+    SectionMonth *sectionMonth = nil;
+    [dateMonthFormatter setDateFormat:@"MMMM"];
+    
+    for (Student *currentStudent in arrayStudents)
+    {
+        NSString *month = [dateMonthFormatter stringFromDate:currentStudent.dayOfBirth];
+        
+        if(!mArraySectionMonth)
+        {
+            mArraySectionMonth = [[NSMutableArray alloc]init];
+        }
+        
+        if(![month isEqualToString:currentMonth])
+        {
+            sectionMonth = [[SectionMonth alloc]init];
+            sectionMonth.titleOfHeader = month;
+            [mArraySectionMonth addObject:sectionMonth];
+            mArrayItems = [[NSMutableArray alloc]init];
+            currentMonth = month;
+        }
+        [mArrayItems addObject:currentStudent];
+        sectionMonth.arrayItems = [[NSArray alloc]initWithArray:mArrayItems];
+    }
+    return[[NSArray alloc]initWithArray:mArraySectionMonth];
+}
+
+-(NSArray*)sortArrayFromArray:(NSArray*)arrayStudents withFilter:(NSString*)filterStr{
+    NSMutableArray<Student*> *mArrayStudents = [[NSMutableArray alloc]init];
+    if([filterStr length] > 0)
+    {
+        for (Student *currentStudent in arrayStudents)
+        {
+            BOOL containInFirstName = [currentStudent.firstName localizedCaseInsensitiveContainsString:filterStr];
+            BOOL containInLastName =[currentStudent.lastName localizedCaseInsensitiveContainsString:filterStr];
+            if(containInFirstName | containInLastName)
+            {
+                [mArrayStudents addObject:currentStudent];
+            }
+        }
+    }
+    else{
+        mArrayStudents = [[NSMutableArray alloc]initWithArray:self.arrayStudents];
+    }
+    NSArray *arrayFiltredStudents = [[NSArray alloc]initWithArray:mArrayStudents];
+    return [self sortArrayFromArray:arrayFiltredStudents];
 }
 
 
